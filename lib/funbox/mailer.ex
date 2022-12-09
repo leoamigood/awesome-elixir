2fe606d8d8a410858901883ad26b6bdb85857ad5
Leo Amigood <leo@amigood.com>
2022-12-09 23:55:49 +0100

    Add credo and dialyzer for code style checking

--- lib/funbox/mailer.ex
+++ lib/funbox/mailer.ex
@@ -1,3 +1,5 @@
 defmodule Funbox.Mailer do
   use Swoosh.Mailer, otp_app: :funbox
+
+  @moduledoc false
 end
--- mix.exs
+++ mix.exs
@@ -44,6 +44,8 @@ defmodule Funbox.MixProject do
       {:phoenix_live_dashboard, "~> 0.6"},
       {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
